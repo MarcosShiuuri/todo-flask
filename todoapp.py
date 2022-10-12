ea9ef67a446c615f445c56b5dd34a693fa4c7c5b
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-10-12 18:39:05 -0300

    Updated code
    Added a database logic

--- todoapp.py
+++ todoapp.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template as rt, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route("/", methods=["POST", "GET"])
 def homepage():
-    return render_template("default.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print("NÃO DEU PARA ADICIONAR!")
+    else:
+        return rt("default.html")
