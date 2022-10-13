30902bdc36d9ccb717a1010d8319b3ab52cd7881
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-10-13 04:55:31 -0300

    Updated the code
    A better version of the previous code

--- todoapp.py
+++ todoapp.py
@@ -1,27 +1,10 @@
 from flask import Flask, render_template as rt, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, instance_relative_config=True)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
+app = Flask(__name__)
 @app.route("/", methods=["POST", "GET"])
 def homepage():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            print("NÃO DEU PARA ADICIONAR!")
+    if request.method =="POST":
+        print("Retornou um submit de POST")
+        return redirect("/")
     else:
-        return rt("default.html")
+        return rt("index.html")
