c8af25fca74a587a6d90824456f2e39eb85d97d1
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-10-18 19:01:34 -0300

    Updated the code

--- todoapp.py
+++ todoapp.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template as rt, request, redirect
-from flask_sqlalchemy import SQLAlchemy as sql
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_flask.db'
-db = sql(app)
+db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
     if request.method == 'POST':
         print(request.form['task_name'])
-        redirect('/')
+        return redirect('/')
     else:
         return rt('index.html')
