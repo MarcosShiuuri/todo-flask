4dfcd50eed7d18fc6d500671a565478b92303a71
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-10-18 19:57:28 -0300

    Updated the code

--- todoapp.py
+++ todoapp.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template as rt, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_flask.db'
 db = SQLAlchemy(app)
+db.init_app(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=dt.utcnow)
+    def __repr__(self):
+        return '<Task %r>' % self.id
+with app.app_context():
+    db.create_all()
 
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
