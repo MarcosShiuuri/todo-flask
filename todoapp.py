d12bb9931fe23217a09c14e626639154e2ccde09
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-10-28 05:25:04 -0300

    CRUD completed

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template as rt, request, redirect
-from flask_sqlalchemy import SQLAlchem
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_flask.db'
